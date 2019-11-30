5a9a5a8476a5ff2ed71b2ea43d6154027f17bfd7
nourhj99 <45139660+nourhj99@users.noreply.github.com>
2019-11-30 15:08:51 +0100

    Add files via upload

--- ToDo.js
+++ ToDo.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 const ToDo = ({list}) => {
   
+  
+    const handleOnClick=(e) => {
+        console.log(e.target.style)
+        e.target.style.textDecoration = "line-through"
+    }
     const ToDoLists = list.length ? (
         list.map(index => {
             return (
                 <div>
-                    <li key={index.id} style={{marginLeft: 100}}  >
+                    <li key={index.id} style={{marginLeft: 100}} onClick={handleOnClick} >
                         {index.content}
                     </li>
                 </div>
